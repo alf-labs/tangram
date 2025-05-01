61f99bafaccf96e252841c94b94149c4d90b0b5f
ralf <ralfoide@gmail.com>
2025-05-01 12:56:58 -0700

    Tangram: Generator, debug generator progress.

--- gen.py
+++ gen.py
@@ -14,6 +14,10 @@ from img_proc import Cell
 from typing import Generator
 from typing import Tuple
 
+DEBUG_MAX_PIECE=12
+DEBUG_SAVE=True
+REPORT_FILE="temp.txt"
+
 PX_CELL_SIZE = 30
 
 PIECES = {
@@ -32,6 +36,19 @@ PIECES = {
             [ (0, 0, 0), (0, 0, 1), (0, 1, 0), (1, 1, 1), (1, 1, 0), (1, 0, 1), ],
         ],
         "rot": False,
+    },
+    "TO": {
+        "color": "Orange",
+        "cells": [
+            [ (0, 0, 0), (0, 0, 1), (0, 1, 0), ],
+        ],
+    },
+    "TY": {
+        "color": "Yellow",
+        "cells": [
+            [ (0, 0, 0), (0, 0, 1), (0, 1, 0), ],
+        ],
+        "count": 2,
     },
     "i": {
         "color": "Red",
@@ -74,19 +91,6 @@ PIECES = {
             [ (0, 0, 0), (0, 0, 1), (0, 1, 0), (1, 2, 0), (1, 1, 1), (1, 1, 0), ],
         ],
     },
-    "TO": {
-        "color": "Orange",
-        "cells": [
-            [ (0, 0, 0), (0, 0, 1), (0, 1, 0), ],
-        ],
-    },
-    "TY": {
-        "color": "Yellow",
-        "cells": [
-            [ (0, 0, 0), (0, 0, 1), (0, 1, 0), ],
-        ],
-        "count": 2,
-    },
 }
 
 class Generator:
@@ -97,17 +101,23 @@ class Generator:
         self.img_count = 0
         self.generated_images = []
         self.gen_count = 0
+        self.gen_failed = 0
+        self.perm_count = 0
+        self.report_file = None
 
     def generate(self, overwrite:bool) -> None:
         print("------")
         print(f"Generating solutions in {self.output_dir_path}")
         print("------")
 
-        self.size_px, self.yrg_coords, cells_empty = self.create_cells()
-        for cells in self.gen_all_solutions(cells_empty):
-            img = self.draw_cells_into(cells, dest_img=None)
-            self.write_indexed_img(img)
+        with open(REPORT_FILE, "w") as self.report_file:
+            self.size_px, self.yrg_coords, cells_empty = self.create_cells()
+            for cells in self.gen_all_solutions(cells_empty):
+                img = self.draw_cells_into(cells, dest_img=None)
+                self.write_indexed_img(img)
 
+        print("")
+        print(f"Stats: permutations={self.perm_count}, gen calls={self.gen_count}, images={self.img_count}")
         print("")
 
     def create_cells(self) -> Tuple[YRGCoord, list[Cell]]:
@@ -167,11 +177,12 @@ class Generator:
 
     def write_indexed_img(self, in_img:np.array) -> None:
         name = "gen_%06d.jpg" % self.img_count
-        self.img_count += 1
-        path = os.path.join(self.output_dir_path, name)
-        cv2.imwrite(path, in_img)
         self.generated_images.append(name)
-        print("@@ Saved", path)
+        self.img_count += 1
+        if DEBUG_SAVE:
+            path = os.path.join(self.output_dir_path, name)
+            cv2.imwrite(path, in_img)
+            print("@@ Saved", path)
 
     def find_cell(self, cells:list[Cell], y_piece:int, r_piece:int, g_piece:int) -> Cell:
         """Returns the cell (if valid) or None."""
@@ -228,28 +239,12 @@ class Generator:
             yrg_rot.append( (y, r, g) )
         return yrg_rot
 
-    # def gen_all_white_pieces(self, cells:list[Cell]) -> Generator:
-    #     """
-    #     Generate all the locations possible for the white cell as non-rotated.
-    #     This is the "key" that identified different puzzles, which is why we never
-    #     rotate the white piece.
-    #     """
-    #     tw = PIECES["TW"]
-    #     # g value of the first cell
-    #     tw_g = tw["cells"][0][2]
-    #     n2 = coord.N//2
-    #
-    #     for y, r, g in coord.VALID_YRG:
-    #         # Only starts on cells with the same g value
-    #         if g == tw_g:
-    #             new_cells = self.place_piece(cells, tw, y - n2, r - n2, angle_deg=0)
-    #             if new_cells is not None:
-    #                 yield new_cells
-
-    def gen_pieces_list(self) -> Generator:
+    def gen_pieces_list(self, max_num_pieces:int=0) -> Generator:
         """
         Generate all the combinations of pieces we want to place, and their
         rotation, but without indicating where to place them.
+
+        max_num_pieces: for debugging purposes to limit the number of pieces.
         """
         pieces = []
         for key, properties in PIECES.items():
@@ -257,7 +252,10 @@ class Generator:
             rotate = properties.get("rot", True)
             cells = properties["cells"]
             for i in range(0, count):
-                pieces.append( {"key": key, "idx": count, "cells": cells, "rot": rotate} )
+                pieces.append( {"key": key, "idx": i, "cells": cells, "rot": rotate} )
+        if max_num_pieces > 0:
+            pieces = pieces[:max_num_pieces]
+        print("@@ Number of pieces in permutations:", len(pieces))
 
         def _gen(_pieces, _current):
             if len(_pieces) == 0:
@@ -280,11 +278,28 @@ class Generator:
         yield from _gen(pieces, [])
 
     def gen_all_solutions(self, cells:list[Cell]) -> Generator:
-        for combo_pieces_rots in self.gen_pieces_list():
-            print("@@ combo_pieces_rots:", combo_pieces_rots)
-            yield from self.place_first_piece(cells, combo_pieces_rots, [])
+        for num_debug in range(1,12):
+            self.img_count = 0
+            p1 = self.perm_count
+            i1 = self.img_count
+            g1 = self.gen_count
+            f1 = self.gen_failed
+            for permutations in self.gen_pieces_list(num_debug):
+                self.perm_count += 1
+                print("@@ permutation", self.perm_count, [ f"{x['key']} @ {x['angle']}" for x in permutations ] )
+                yield from self.place_first_piece(cells, permutations, "")
+            p2 = self.perm_count - p1
+            i2 = self.img_count - i1
+            g2 = self.gen_count - g1
+            f2 = self.gen_failed - f1
+            r = f"@@ DEBUG num permutations: pieces={num_debug} perms_count={p2} gen_count={f2} / {g2} img_count={i2}"
+            self.report_file.write(r)
+            self.report_file.write("\n")
+            print(r)
+            if num_debug == DEBUG_MAX_PIECE:
+                break
 
-    def place_first_piece(self, cells:list[Cell], combos:list, current:list) -> Generator:
+    def place_first_piece(self, cells:list[Cell], combos:list, current:str) -> Generator:
         if len(combos) == 0:
             return
         info = combos.pop(0)
@@ -303,18 +318,23 @@ class Generator:
                 # print(f"@@ gen {self.img_count} {combos}, {current} + {key} @ {angle_deg}:{y}x{r}")
                 new_cells = self.place_piece(cells, piece_cells, piece, y - n2, r - n2, angle_deg)
                 self.gen_count += 1
-                if new_cells is not None:
+                # new_current = f"{current}{key}@{angle_deg}:{y}x{r} "
+                new_current = current
+                if new_cells is None:
+                    self.gen_failed += 1
+                else:
                     if len(combos) == 0:
-                        print("@@ gen", self.gen_count, "--", self.img_count , "found")
+                        # print("@@ GEN", self.gen_failed, "/", self.gen_count, "--", self.img_count, new_current, "FOUND")
                         yield new_cells
                     else:
                         new_combos = combos.copy()
-                        # new_current = current.copy()
-                        # new_current.append(f"{key} @ {angle_deg}:{y}x{r}")
                         # print(f"@@ gen {self.img_count} {new_combos}, {new_current}")
+                        # print(f"@@ gen {self.img_count}: {new_current}")
                         # print("@@ gen", self.img_count, new_combos)
-                        print("@@ gen", self.gen_count, "--", self.img_count , end="\r")
-                        yield from self.place_first_piece(new_cells, new_combos, current)
+                        # print("@@ SUB", self.gen_failed, "/", self.gen_count, "--", self.img_count, new_current, end="\r")
+                        # print("@@ SUB", self.gen_failed, "/", self.gen_count, "--", self.img_count, new_current, end="\r")
+                        print("@@ SUB", self.gen_failed, "/", self.gen_count, "--", self.img_count, end="\r")
+                        yield from self.place_first_piece(new_cells, new_combos, new_current)
 
 
 # ~~
