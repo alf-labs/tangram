48849dfa91227b78c8f3bb7b5cf140187af73a8e
ralf <ralfoide@gmail.com>
2025-05-09 16:14:23 -0700

    Tangram Viewer: Update build info.

--- viewer/src/GitBuild.tsx
+++ viewer/src/GitBuild.tsx
@@ -1,2 +1,2 @@
-export const GIT_HASH_STR = "main @ 1f36dfe";
-export const GIT_LONG_STR = "Build main @ 1f36dfe, 2025-05-09 16:09:19 -0700";
+export const GIT_HASH_STR = "main @ 1a275c7";
+export const GIT_LONG_STR = "Build main @ 1a275c7, 2025-05-09 16:14:23 -0700";
