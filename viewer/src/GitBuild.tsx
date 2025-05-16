f0ccf2fa5253bef15174d478c47829ca2014c0b7
ralf <ralfoide@gmail.com>
2025-05-16 16:16:22 -0700

    Tangram Viewer: Version 1.6

--- viewer/src/GitBuild.tsx
+++ viewer/src/GitBuild.tsx
@@ -1,4 +1,4 @@
-export const GIT_HASH_STR = "main @ 636febb";
-export const GIT_LONG_STR = "Build main @ 636febb, 2025-05-10 10:37:05 -0700";
+export const GIT_HASH_STR = "main @ 420ceac";
+export const GIT_LONG_STR = "Build main @ 420ceac, 2025-05-16 16:15:11 -0700";
 export const VERSION_MAJOR = "1";
-export const VERSION_MINOR = "5";
+export const VERSION_MINOR = "6";
