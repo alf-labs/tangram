c254cd70765f59a21755d696431ee975877769bf
ralf <ralfoide@gmail.com>
2025-05-16 00:13:24 -0700

    Tangram RGen: Use a producer/consumer pattern with channels to run on multiple threads.

--- rgen/src/main.rs
+++ rgen/src/main.rs
@@ -1,8 +1,13 @@
-use crate::pieces::Pieces;
-use clap::{Parser, Subcommand};
-use std::ops::RangeInclusive;
 use crate::coord::Coords;
-use crate::rgen::RGen;
+use crate::pieces::Pieces;
+use crate::rgen::{RGen, RGenResult};
+use clap::{Parser, Subcommand};
+use std::fs::OpenOptions;
+use std::io::Write;
+use std::ops::RangeInclusive;
+use std::sync::{mpsc, Arc};
+use std::sync::mpsc::{Receiver, Sender};
+use std::thread;
 
 mod macros;
 mod coord;
@@ -34,12 +39,21 @@ struct Cli {
 #[derive(Subcommand)]
 enum Commands {
     Dump {
-
         #[arg(long,
         help = "Print everything (very verbose).")]
         print_all: bool,
     },
     Gen {
+        // #[arg(long, default_value_t = 0,
+        //     help = "Generator core index from 0 to gen-cores-1.")]
+        // gen_index: i32,
+
+        #[arg(long, default_value_t = 1,
+            help = "Generator number of cores.")]
+        gen_cores: i32,
+
+        #[arg(short = 'o', long, value_name = "FILE", default_value = "rgen_output.txt")]
+        gen_output: String,
     }
 }
 
@@ -49,22 +63,76 @@ fn main() {
     let cli = Cli::parse();
     let range = RangeInclusive::new(cli.min_perm, cli.max_perm);
 
-    let coord = Coords::new();
-    let pieces = Pieces::new(&coord);
-
     match &cli.command {
         Some( Commands::Dump { print_all  } ) => {
+            let coords = Coords::new();
+            let pieces = Pieces::new(&coords);
             pieces.dump(&cli.piece, range, *print_all)
         },
-        Some( Commands::Gen { } ) => {
-            let mut rgen = RGen::new(
-                &pieces,
-                &coord,
-                Some("rgen_output.txt"));
-            rgen.run(&cli.piece, range)
+        Some( Commands::Gen { gen_cores, gen_output } ) => {
+            executor(cli.piece, range, *gen_cores, gen_output);
         },
         &None => {}
     }
 }
+
+fn executor(piece_selector: i32,
+            perm_range: RangeInclusive<i32>,
+            gen_cores: i32,
+            gen_output: &String) {
+    let mut file = OpenOptions::new()
+        .append(true)
+        .create(true)
+        .open(gen_output).expect("Could not open output file");
+
+    let coords = Arc::new( Coords::new() );
+    let pieces = Arc::new( Pieces::new(&coords) );
+
+    let (tx, rx): (Sender<RGenResult>, Receiver<RGenResult>) = mpsc::channel();
+
+    // Receiver Thread
+    let receiver = thread::spawn(move || {
+        let mut sig_count = 0;
+        for message in rx {
+            if message.result.is_empty() {
+                // This is a progress log.
+                println!("[{:?}] {}",
+                         message.tid,
+                         message.progress.replace("CNT", sig_count.to_string().as_str()));
+            } else {
+                // This is a solution signature
+                sig_count += 1;
+                writeln!(file, "{}", message.result).expect("Could not write to file");
+                file.sync_all().expect("Could not sync file");
+            }
+        }
+    });
+
+    // Producer Threads
+    let mut receivers = Vec::new();
+    for i in 0..gen_cores {
+        let tx_clone = tx.clone();
+        let pieces_clone = pieces.clone();
+        let coords_clone = coords.clone();
+        let range_clone = perm_range.clone();
+        let receiver = thread::spawn(move || {
+            let mut rgen = RGen::new(
+                &pieces_clone,
+                &coords_clone,
+                &tx_clone);
+            rgen.run(piece_selector, &range_clone, i, gen_cores)
+        });
+        receivers.push(receiver);
+    }
+
+    receiver.join().expect("Could not join receiver thread");
+    for receiver in receivers {
+        receiver.join().expect("Could not join receiver thread");
+    }
+
+    println!("Done");
+}
+
+
 
 // ~~
--- rgen/src/rgen.rs
+++ rgen/src/rgen.rs
@@ -3,57 +3,92 @@ use crate::coord::Coords;
 use crate::permutations::Permutations;
 use crate::pieces::Pieces;
 use crate::solutions::BoardSolution;
-use std::fs::{File, OpenOptions};
-use std::io::Write;
 use std::ops::RangeInclusive;
+use std::string::String;
+use std::sync::mpsc::Sender;
+use std::thread;
+use std::thread::ThreadId;
 use std::time::Instant;
+
+// Send a log about every 2 seconds. Keep it a power of 2. Use a lower value for debug builds.
+#[cfg(debug_assertions)]
+const PROGRESS_MOD: i32 = 64;
+#[cfg(not(debug_assertions))]
+const PROGRESS_MOD: i32 = 1024;
+
+
+#[derive(Clone)]
+pub struct RGenResult {
+    pub tid: ThreadId,
+    pub progress: String,
+    pub result: String,
+}
+
+impl RGenResult {
+    fn new_result(board: &Board, sol: &BoardSolution) -> RGenResult {
+        RGenResult {
+            tid: thread::current().id(),
+            progress: String::new(),
+            result: format!("@@ [{}] SIG {} {} # {:?}", board.perm_index, board, sol, thread::current().id()),
+        }
+    }
+
+    fn new_progress(log: String) -> RGenResult {
+        RGenResult {
+            tid: thread::current().id(),
+            progress: log,
+            result: String::new(),
+        }
+    }
+}
+
 
 pub struct RGen<'a> {
     pieces: &'a Pieces,
     coords: &'a Coords,
-    output_file: Option<File>,
-    output_count: i32,
+    sender: &'a Sender<RGenResult>,
 }
 
 impl RGen<'_> {
     pub fn new<'a>(pieces: &'a Pieces,
                    coords: &'a Coords,
-                   file_path: Option<&str>) -> RGen<'a> {
-        let file = match file_path {
-            None => { None }
-            Some(path) => Option::from({
-                OpenOptions::new()
-                    .append(true)
-                    .create(true)
-                    .open(path).expect("Could not open output file")
-            })
-        };
+                   sender: &'a Sender<RGenResult>) -> RGen<'a> {
         RGen {
             pieces,
             coords,
-            output_file: file,
-            output_count: 0,
+            sender,
         }
     }
 
     pub fn run(&mut self,
-               piece_selector: &i32,
-               perm_range: RangeInclusive<i32>) {
+               piece_selector: i32,
+               perm_range: &RangeInclusive<i32>,
+               gen_index: i32,
+               gen_modulo: i32) {
         let pmin = *perm_range.start();
         let pmax = *perm_range.end();
         let start_ts = Instant::now();
         let mut spd = 0.0;
         let mut perm_count = 0;
 
-        for permutations in self.pieces.iter(*piece_selector) {
+        for permutations in self.pieces.iter(piece_selector) {
             let index = permutations.index;
             if index < pmin {
                 continue
             } else if index > pmax {
                 break
             }
-            println!("@@ perm {0}, {1:.2} pps, img {2}, {3}",
-                index, spd, self.output_count, permutations);
+            if gen_modulo > 1 {
+                if index % gen_modulo != gen_index {
+                    continue;
+                }
+            }
+
+            if perm_count % PROGRESS_MOD == 0 {
+                self.sender.send(RGenResult::new_progress(
+                    format!("@@ perm {0}, {1:.2} pps, img CNT, {2}", index, spd, permutations)
+                )).expect("Failed to send progress");
+            }
 
             let empty_board = Board::new(permutations.index, self.coords);
             self.place_pieces(empty_board, permutations, BoardSolution::new());
@@ -66,16 +101,7 @@ impl RGen<'_> {
     }
 
     fn emit_solution(&mut self, new_board: &Board, new_sol: &BoardSolution) {
-        match self.output_file {
-            None => {}
-            Some(ref mut file) => {
-                writeln!(file, "@@ [{}] SIG {} {}", new_board.perm_index, new_board, new_sol)
-                    .expect("Failed to write to output file");
-                file.sync_all()
-                    .expect("Failed to sync output file");
-                self.output_count += 1;
-            }
-        }
+        self.sender.send(RGenResult::new_result(new_board, new_sol)).expect("Failed to send solution");
     }
 
     fn place_pieces(&mut self,
