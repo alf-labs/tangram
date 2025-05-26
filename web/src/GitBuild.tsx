553c0588bfd18b0a17616c79af0e62db94ef71f9
ralf <ralfoide@gmail.com>
2025-05-26 11:10:35 -0700

    Tangram Web: Update git build info.

--- web/src/GitBuild.tsx
+++ web/src/GitBuild.tsx
@@ -1,4 +1,4 @@
-export const GIT_HASH_STR = "main @ ff53546";
-export const GIT_LONG_STR = "Build main @ ff53546, 2025-05-25 14:32:23 -0700";
+export const GIT_HASH_STR = "main @ 09bb8db";
+export const GIT_LONG_STR = "Build main @ 09bb8db, 2025-05-26 11:03:09 -0700";
 export const VERSION_MAJOR = "2";
 export const VERSION_MINOR = "0";
